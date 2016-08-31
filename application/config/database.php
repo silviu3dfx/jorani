14970b6d32d4ebeb12b5c291a940d83560120897
derTuca <alex.tuca@koding.ro>
2016-08-31 20:02:03 +0300

    scos text care sa "simbolizeze" succesul
    Text useless din snippetul de la Microsoft; scos

--- application/config/database.php
+++ application/config/database.php
@@ -69,8 +69,6 @@ if (!$link) {
     exit;
 }
 
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
 
 mysqli_close($link);
 $active_group = 'default';
